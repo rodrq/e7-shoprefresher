b1e46fa36b8428509fbf0d61b07a64d117a37fb4
Rodri Rooney <rodrqq@gmail.com>
2023-03-21 04:41:59 -0300

    locate function

--- refresher.py
+++ refresher.py
@@ -7,22 +7,30 @@ def adb_command(text:str):
     return subprocess.run([word for word in text.split()])
 
 def get_screenshot():
-    adb_command('adb shell screencap -p /sdcard/screenshot.png')
-    adb_command('adb pull /sdcard/screenshot.png screenshot.png')
+    with subprocess.Popen("adb shell screencap -p", stdout=subprocess.PIPE, shell=True) as proc: 
+        image_bytes = proc.stdout.read().replace(b'\r\n', b'\n')
+        image = cv.imdecode(np.frombuffer(image_bytes, np.uint8), cv.IMREAD_COLOR)
+        return image
 
-def locate(media, threshold:float):
-    screenshot = cv.imread('screenshot.png')
+def locate(media, threshold:float=0.75):
+    screenshot = get_screenshot()
     template = cv.imread(f"media/{media}.png")
     result = cv.matchTemplate(screenshot, template, cv.TM_CCOEFF_NORMED)
     min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
-    print(max_val)
+    w, h = template.shape[:-1]
+    center = (int(max_loc[0] + w/2), int(max_loc[1] + h/2))
     if max_val >= threshold:
-        return True
-    elif max_val < threshold:
-        return False
+        return center
+    else:
+        print('Image not found')
+        return None
 
 
-def buy():
-    if locate('mystics', 0.8):
+
+def buy(media):
+    get_coords = locate(media)
+    if get_coords:
+        pass
+    else:
         pass
 
